69be587df904d95d6ce69b891e46a6e3112d5e3c
bensondaled <bendeverett@gmail.com>
2016-04-13 17:34:15 -0400

    update

--- tcpip_read.m
+++ tcpip_read.m
@@ -8,10 +8,23 @@ function tcpip_read(srvr, event)
     %disp(msg)
     
     siHandle = evalin('base', 'hSI');
+    
+    while siHandle.active==1
+        waitfor(msgbox('ScanImage is running an acqusition while TCPIP attempted to communicate. Abort, then click okay.'))
+    end
+    
     siHandle.hScan2D.logFilePath = msg.path;
     siHandle.hScan2D.logFileStem = msg.name;
-    siHandle.hScan2D.logFileCounter = msg.count;
+    siHandle.hScan2D.logFileCounter = msg.idx;
+    
+    % defaults
+    siHandle.hScan2D.logFramesPerFile = 1000;
+    siHandle.hStackManager.framesPerSlice = inf;
+    siHandle.acqsPerLoop = 10000;
+    siHandle.extTrigEnable = 1;
+    
+    siHandle.startLoop;
     
     fwrite(srvr, '_ok');
-    tcpip_reconnect;
+    tcpip_reconnect(0,0);
 end
